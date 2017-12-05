94e5ac599a13b15f55666be96616d044daf0cba7
mefistoduan <838560574@qq.com>
2017-12-05 22:32:41 +0800

    3

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,6 @@
 <template>
   <div id="app">
     <div class="page page-current">
-      <nav class="bar bar-tab">
-        <router-link :to="{ path: '/'}" label="首页" class="tab-item external" v-bind:class='{ "active" : isA}'  >
-          <span class="icon icon-home" @click="navbar(1)"></span>
-          <span class="tab-label">home</span>
-        </router-link>
-        <router-link :to="{ path: '/tasks'}" label="订单" class="tab-item external " v-bind:class='{ "active" : isB}'>
-          <span class="icon icon-menu" @click="navbar(2)"></span>
-          <span class="tab-label">tasks</span>
-        </router-link>
-        <router-link :to="{ path: '/me'}" label="我的" class="tab-item external " v-bind:class='{ "active" : isC}'>
-          <span class="icon icon-me" @click="navbar(3)"></span>
-          <span class="tab-label">me</span>
-        </router-link>
-      </nav>
-
       <router-view></router-view>
     </div>
   </div>
@@ -31,107 +16,11 @@
       }
     },
     methods:{
-        navbar:function (value) {
-        this.isA = false,
-        this.isB = false,
-        this.isC = false
-        if(value == 1){
-          this.isA = true
-        };
-        if(value == 2){
-          this.isB = true
-        };
-        if(value == 3){
-          this.isC = true
-        }
-      }
     },
     components: {
     }
   }
 </script>
 <style>
-  .bar-tab , .bar-tab .tab-item:active {
-    color:#797979;
-  }
-  .content-block,.content {
-    background-color: #fff;
-    font-family: "Microsoft Yahei", "å¾®è½¯é›…é»‘";
-  }
-  .content-block {
-    margin-top: 0.875rem;
-  }
-  .active .tab-label {
-    color: #FF9545;
-  }
-  .tab-item.active .icon {
-    color: #FF9545;
-  }
-  .back_btn {
-    width: 15px;
-    height: 100%;
-    overflow: hidden;
-    float: left;
-    position: relative;
-    z-index: 20;
-    font-size: 1rem;
-    line-height: 1.2rem;
-  }
-  .back_btn img {
-    margin-top: 15px;
-  }
-  .close_btn {
-    float: right;
-    width: 15px;
-    height: 100%;
-    overflow: hidden;
-    position: relative;
-    z-index: 20;
-    font-size: 1rem;
-    line-height: 1.2rem;
-    display: none;
-  }
-  .close_btn img {
-    margin-top: 15px;
-  }
-  header.bar {
-    background-color:#FF9545;
-    color: #fff;
-    background-image: -moz-linear-gradient( 105deg, rgb(255,101,56) 0%, rgb(255,131,77) 100%);
-    background-image: -webkit-linear-gradient( 105deg, rgb(255,101,56) 0%, rgb(255,131,77) 100%);
-    background-image: -ms-linear-gradient( 105deg, rgb(255,101,56) 0%, rgb(255,131,77) 100%);
-
-  }
-  .wash_title {
-    font-size: 16px;
-    font-weight: 500;
-    color: #fff;
-    line-height: 44px;
-    word-spacing:8px;
-    letter-spacing: 3px;
-  }
-  .search_icon {
-    float: right;
-  }
-  .search_icon img {
-    margin-top: 15px;
-    margin-right: 10px;
-    position: relative;
-    z-index: 20;
-    font-size: 1rem;
-    line-height: 1.2rem;
-  }
-  em, i {
-    font-style: normal;
-  }
-  ul,li {
-    padding: 0;
-    margin: 0;
-    list-style: none;
-  }
-  /*åŠ©æ‰‹æ */
-  .bar-tab .tab-item .icon {
-    width: 28px;
-  }
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/views/index'
-import tasks from '@/views/tasks'
-import me from '@/views/me'
-import coupon from '@/views/coupon'
-import collect from '@/views/collect'
-import history_appraise from '@/views/history_appraise'
-import help from '@/views/help'
-import appoint from '@/views/appoint'
-import set_up from '@/views/set_up'
-import test from '@/views/test'
 
 Vue.use(Router)
 
@@ -21,50 +12,5 @@ export default new Router({
       name: 'index',
       component: index
     },
-    {
-      path: '/tasks',
-      name: 'tasks',
-      component: tasks
-    },
-    {
-      path: '/me',
-      name: 'me',
-      component: me
-    },
-    {
-      path: '/coupon',
-      name: 'coupon',
-      component: coupon
-    },
-    {
-      path: '/collect',
-      name: 'collect',
-      component: collect
-    },
-    {
-      path: '/history_appraise',
-      name: 'history_appraise',
-      component: history_appraise
-    },
-    {
-      path: '/help',
-      name: 'help',
-      component: help
-    },
-    {
-      path: '/appoint',
-      name: 'appoint',
-      component: appoint
-    },
-    {
-      path: '/set_up',
-      name: 'set_up',
-      component: set_up
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: test
-    }
   ]
 })
